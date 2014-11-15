6ccd21c4d41fe69c85a3211aa7fdc81cbcc1e94e
renyedi <renyedi@ebay.com>
2014-11-14 23:46:45 -0800

    Minute 58

--- tableViewDemo/ViewController.swift
+++ tableViewDemo/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     @IBOutlet weak var tableView: UITableView!
     var users:[[String:String]]!
-    
+    var isExpanded: [Int:Bool]! = [Int:Bool]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +26,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         tableView.dataSource = self
         tableView.delegate = self
+        tableView.rowHeight = 107
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,8 +34,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // Dispose of any resources that can be recreated.
     }
 
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 10
+    }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return users.count
+        if let expanded = isExpanded[section] {
+            return expanded ? users.count : 1
+        } else {
+            return 1
+        }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -46,6 +55,34 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         cell.hometownLabel.text = user["hometown"]
         
         return cell
+    }
+    
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        var headerView = UIView(frame:CGRect(x: 0, y: 0, width: 320, height: 50))
+        headerView.backgroundColor = UIColor(white: 0.8, alpha: 0.8)
+        
+        var headerLabel = UILabel(frame:CGRect(x: 10, y: 0, width: 320, height: 50))
+        headerLabel.text = "Section \(section)"
+        headerView.addSubview(headerLabel)
+        
+        return headerView
+    }
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+
+        if let expanded = isExpanded[indexPath.section] {
+            isExpanded[indexPath.section] = !expanded
+        } else {
+            isExpanded[indexPath.section] = true
+        }
+        
+        tableView.reloadData()
+        tableView.reloadSections(NSIndexSet(index: indexPath.section), withRowAnimation: UITableViewRowAnimation.Fade)
     }
 }
 
